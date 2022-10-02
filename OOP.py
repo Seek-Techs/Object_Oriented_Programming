376d06edbac9dd864cee83fb7eae0367c159b7c5
Seek-Techs <sikiru.yusuff@yahoo.com>
2022-10-02 19:49:51 +0100

    update

--- OOP.py
+++ OOP.py
@@ -1,54 +1,68 @@
 import datetime
 
 class Account:
-    account_balance = 3000
-    current_local = str(datetime.datetime.now())
-    def __init__(self, name: str, account_number: str):
+    
+    # Define attributes through an Instance
+    def __init__(self, name:str, account_num:int):
         self.name = name
-        self.account = account_number
-        #self.current_date = current_date
+        self.account_num = account_num
+        self.balance = 0.0
+        self.current_local = str(datetime.datetime.now())
 
+    # Methods to perform on the Account are as defined below:
     def show_menu(self):
-        
-        msg = {
-            'Acount Name': self.name, 'Account balance': Account.account_balance, 
-            'Current Date': Account.current_local
-            }
-            
+        msg = 'Welcome to CeayHollertech Bank, %s\n\nYour Current Balance: $%s\nAccount Number: %s\nDate: %s\n' %(
+            self.name, self.balance, self.account_num, self.current_local
+            )
+        return msg
+    
+    def get_balance(self):
+        msg = 'Your Balance is $%s ' %(self.balance)
         return msg
 
-    def get_balance(self):
-        available_balance = f'Available Balance: {Account.account_balance}\n'
-        return available_balance
-
-    def deposit(self, amount: int):
-        Account.account_balance 
-        Account.account_balance += amount
-        deposited = 'Deposited Successful!' #%(amount) %(Account.account_balance + amount)
-        return deposited
-
-    def withdraw(self, amount_withdraw: int):
-        available_balance = Account.account_balance
-        available_balance = available_balance - amount_withdraw
-        if amount_withdraw > available_balance:
-            msg = "insufficient funds. Your current balance: %s  " % (Account.account_balance)
+    def deposit(self, amount:float):
+        self.balance += amount
+        msg = 'Deposited Succesfully!\nYour Account Balance is $%s \n' %(self.balance)
+        return msg
+    
+    def withdraw(self, amount: float):
+        if self.balance > amount:
+            self.balance -= amount
+            msg = 'Withdrawal Successful!\nYour Balance is $%s\n ' %(self.balance)
             return msg
+        
+        return 'Insufficient Balance'
+    
+    def transfer(self, account: int, amount: float):
+        if self.balance > amount:
+            self.balance -= amount
+            account.balance += amount
+            msg = 'Transfer from %s to %s\nDate: %s\nAvailable Balance: $%s ' %(
+                self.name, account.name, self.current_local, self.balance
+                )
+            return msg
+        return 'Insufficient Fund! Your Available Balance: $%s ' %(self.balance)
+        
+# Define Account Object
+yusuff = Account('Sikiru Yusuff', '2345678')
 
-        msg = "sufficient funds. Amount withdrew: %s  " % (amount_withdraw)
-        ad = available_balance - amount_withdraw
-        return msg, ad
+# Display menu
+print(yusuff.show_menu())
 
-account = Account('yusuff', 122333333)
-print(account.show_menu())
+# Balance of the account
+print(yusuff.get_balance())
 
-account_balance = account.get_balance()
-print(account_balance)
+# Deposit money to the account
+print(yusuff.deposit(345.78))
 
-deposit = account.deposit(23000)
-print(deposit)
+# Withdraw amount 
+print(yusuff.withdraw(100))
 
-account_balance = account.get_balance()
-print('correct', account_balance)
+# Define an account object to transfer with passing the account name and the number
+queen = Account('Queen Gatty', 3456782)
 
-withdraw = account.withdraw(1000000000)
-print(withdraw)
+# A transfer to the account
+print(yusuff.transfer(queen, 44))
+
+# Confirm account transfer
+print(queen.get_balance())
